21ee24f4cb551d1f8f94164a5ed9b14372bb5c87
Alex <weberale@uni-hildesheim.de>
2019-06-28 17:10:50 +0200

    asd

--- PluginPart/RegressionToolPlugin/src/main/java/io/jenkins/plugins/PluginMenueClass.java
+++ PluginPart/RegressionToolPlugin/src/main/java/io/jenkins/plugins/PluginMenueClass.java
@@ -1,17 +1,55 @@
 package io.jenkins.plugins;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.DataBoundSetter;
 
-public class PluginMenueClass {
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.BuildListener;
+import hudson.tasks.BuildWrapper;
+import hudson.tasks.BuildWrapperDescriptor;
+
+public class PluginMenueClass extends BuildWrapper{
     
     private String pfadZuBasen;
     
+    private String pfadZuBuilds;
+    
     @DataBoundConstructor
-    public PluginMenueClass(String pfadZuBasen) {
+    public PluginMenueClass(String pfadZuBasen, String pfadZuBuilds) {
         this.pfadZuBasen = pfadZuBasen;
+        this.pfadZuBuilds = pfadZuBuilds;
     }
     
     public String getPfadZuBasen() {
         return pfadZuBasen;
+    }
+    
+    public String getPfadZuBuilds() {
+        return pfadZuBuilds;
+    }
+    @Override
+    public Environment setUp(AbstractBuild build,
+            Launcher launcher,
+            BuildListener listener) {
+        return new Environment() {
+        };
+    }
+    
+    @Extension
+    public static class DescriptorImpl extends BuildWrapperDescriptor{
+
+        @Override
+        public boolean isApplicable(AbstractProject<?, ?> item) {
+            // TODO Auto-generated method stub
+            return true;
+        }
+        @Override
+        public String getDisplayName() {
+            return "TestNameHier";
+        }
+        
     }
 }
