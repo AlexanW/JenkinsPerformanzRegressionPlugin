5ab78e14239e737a59638cde090bff596031bdd1
alexa <alexa@LAPTOP-D53G1P03.home>
2019-07-11 22:32:35 +0200

    JUnitTryes

---
+++ PluginPart/RegressionToolPlugin/src/test/java/io/jenkins/plugins/MyTestClass.java
@@ -0,0 +1,13 @@
+package io.jenkins.plugins;
+
+import org.junit.Test;
+
+import testRegression.ErstelleBasis;
+import testRegression.IErstelleBasis;
+import testRegression.TestVergleichen;
+
+public class MyTestClass {
+    @Test
+    public void testBaseCreation() {
+    }
+}
