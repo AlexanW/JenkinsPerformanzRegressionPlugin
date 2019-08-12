b73b5c71b9d88655e7fb63ba58c3e464c1304c4e
alexa <alexa@LAPTOP-D53G1P03.home>
2019-08-12 15:54:35 +0200

    yxc

--- PluginPart/RegressionToolPlugin/target/generated-test-sources/injected/InjectedTest.java
+++ PluginPart/RegressionToolPlugin/target/generated-test-sources/injected/InjectedTest.java
@@ -6,7 +6,7 @@ import java.util.*;
  */
 public class InjectedTest extends junit.framework.TestCase {
   public static junit.framework.Test suite() throws Exception {
-    System.out.println("Running tests for "+"io.jenkins.plugins:RegressionToolPlugin:1.26");
+    System.out.println("Running tests for "+"io.jenkins.plugins:RegressionToolPlugin:1.3");
     Map<String, Object> parameters = new HashMap<String, Object>();
     parameters.put("basedir","C:\\Uni\\SeminareProjekte\\ContinuousIntegrationPerformanz(Bachelor)\\Tool\\CIRegressionTool\\PluginPart\\RegressionToolPlugin");
     parameters.put("artifactId","RegressionToolPlugin");
